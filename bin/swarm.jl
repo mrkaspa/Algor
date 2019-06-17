1e13cbdeb59f4f0d514dfcbb9beddae80ebd67a4
Michel Perez <michel.ingesoft@gmail.com>
2019-06-16 23:06:26 -0500

    swarm no neighbor

--- bin/swarm.jl
+++ bin/swarm.jl
@@ -3,8 +3,9 @@ using Plots
 const x_axis = 50
 const y_axis = 50
 const particle_shape = 0.05
-const bag_w = 20
-const bag_h = 20
+const bag_w = 50
+const bag_h = 50
+const files_path = joinpath(pwd(), "assets", "swarm")
 
 function draw_rect(width, height)
     plot!(Shape(x_axis .+ [0, width, width, 0], y_axis .+ [0, 0, height, height]))
@@ -15,10 +16,13 @@ function draw_particle(x, y)
         y_axis .+ [y - particle_shape, y - particle_shape, y + particle_shape, y + particle_shape]))
 end
 
-function draw(x, y)
+function draw(particles)
     plot()
     draw_rect(bag_w, bag_h)
-    draw_particle(x, y)
+    for p in particles
+        (_, x, y) = p
+        draw_particle(x, y)
+    end
 end
 
 function in_bag(x, y)
@@ -26,30 +30,41 @@ function in_bag(x, y)
 end
 
 function move(x, y)
-    (x + randn() - 0.5, y + randn() - 0.5)
+    (x + randn(), y + randn())
 end
 
-function main()
+function init()
     gr()
-
-    files_path = joinpath(pwd(), "assets", "swarm")
     if isdir(files_path)
         rm(files_path, recursive = true)
     end
     mkdir(files_path)
+end
 
-    x = rand(0:bag_w)
-    y = rand(0:bag_h)
+function main(n)
+    init()
+
+    particles = map(1:n) do id
+        x = 25.0 # Float64(rand(0:bag_w))
+        y = 25.0 # Float64(rand(0:bag_h))
+        println("init point ($x, $y) for $id")
+        (id, x, y)
+    end
     i = 0
-    println("init point ($x, $y)")
-    while in_bag(x, y)
-        println("attempt $i, res = $(in_bag(x, y))")
-        (x, y) = move(x, y)
-        println("next point ($x, $y)")
-        draw(x, y)
+    while all(p->in_bag(p[2], p[3]), particles)
+        println("attempt $i")
+        for p in particles
+            (id, x, y) = p
+            println("particle $id, in bag? = $(in_bag(x, y))")
+            (nx, ny) = move(x, y)
+            println("id $id")
+            particles[id] = (id, nx, ny)
+            println("next point ($nx, $ny)")
+        end
+        draw(particles)
         png(joinpath(files_path, "_$i.png"))
         i += 1
     end
 end
 
-main()
+main(5)
